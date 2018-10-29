a6e53f80c95510cd9cdcc5e9a67460425d04bbb2
Guilherme Almino Carvalho <36975422+GuilhermeIsNotUnix@users.noreply.github.com>
2018-10-29 08:00:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+setInterval(function() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	
+	document.body.style.backgroundColor = color;
+}, 900);
